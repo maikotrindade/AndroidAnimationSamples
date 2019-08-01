d552dcf07c2f9ef956956dda9276cbae3f320b36
maikotrindade <maikocharles@gmail.com>
2019-08-01 16:54:02 -0300

    add lauch icon and setup navigation

---
+++ app/src/main/java/io/github/maikotrindade/animationsamples/ui/MainActivity.kt
@@ -0,0 +1,45 @@
+package io.github.maikotrindade.animationsamples.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import io.github.maikotrindade.animationsamples.R
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setupNavigation()
+    }
+
+    private fun setupNavigation() {
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+        val host: NavHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment?
+            ?: return
+        val navController = host.navController
+        navController.addOnDestinationChangedListener { navController: NavController, _: NavDestination, _: Bundle? ->
+            toolbar.title = navController.currentDestination?.label
+        }
+        appBarConfiguration = AppBarConfiguration(
+            topLevelDestinationIds = setOf(R.id.mainFragment)
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return findNavController(R.id.nav_host_fragment).navigateUp(appBarConfiguration)
+    }
+
+
+}
---
+++ app/src/main/java/io/github/maikotrindade/animationsamples/ui/MainFragment.kt
@@ -0,0 +1,19 @@
+package io.github.maikotrindade.animationsamples.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import io.github.maikotrindade.animationsamples.R
+
+class MainFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onStart() {
+        super.onStart()
+    }
+}
